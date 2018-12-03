ee9d9792febb35760ddd54be6c277afe2a7c545c
xmq291 <xmq291@gmail.com>
2018-12-02 23:38:58 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '264794204208506',
+        clientSecret: 'ed5b0215c2cf1b669fcf62b9274cdb54'
+    }
 }
